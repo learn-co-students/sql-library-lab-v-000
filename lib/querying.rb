334d421aacc42785c6ff357e74e46654ace5eed8
coltonstaab1 <colton.staab@gmail.com>
2016-01-13 18:48:47 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,17 +1,23 @@
 def select_books_titles_and_years_in_first_series_order_by_year
+	"SELECT title, year FROM books WHERE series_id = 1 ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
+	"SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1;"  
 end
 
 def select_value_and_count_of_most_prolific_species
+	"SELECT species, count(*) FROM characters GROUP BY species ORDER BY count(*) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
+	"SELECT authors.name, subgenres.name FROM series JOIN authors ON authors.id = series.author_id JOIN subgenres ON subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
+	"SELECT series.title FROM series JOIN books ON books.series_id = series.id JOIN character_books ON character_books.book_id = books.id JOIN characters ON character_books.character_id = characters.id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
+	"SELECT characters.name, COUNT(books.title) FROM characters JOIN character_books ON characters.id = character_books.character_id JOIN books ON books.id = character_books.book_id GROUP BY characters.name ORDER BY COUNT(books.title) DESC;"
 end
---
+++ lib/update.sql
@@ -0,0 +1,1 @@
+UPDATE characters SET species = 'Martian' WHERE id = (SELECT COUNT(*) FROM characters);
