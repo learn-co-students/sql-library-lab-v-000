b176a3e8a2f0799ee60f6e91da035ab4e5adf109
shmulim <shmuel.markel@gmail.com>
2016-01-31 00:51:45 -0800

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
+
+INSERT INTO subgenres (id, name) VALUES ();
+
+INSERT INTO authors (id, name) VALUES ();
+
+INSERT INTO books (id, title, year, series_id) VALUES ();
+
+INSERT INTO characthers (id, name, species, motto, series_id, author_id) VALUES ();
