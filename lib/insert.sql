88af83a9c734bbb3603b0dec97b303b5934bb445
authorbeard <msnolan@gmail.com>
2016-02-03 12:13:58 -0500

    refactored to shorten code; solution for spec/04_querying_spec.rb:25 seems unnecessarily long.

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,7 +8,7 @@ INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 199
 
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lanister", "A Lanister always pays is debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
 
-INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Kylar Stern", "stern undying dier", "kakarifer", 2, 2), (6, "Devon Corgi", "I don't have a motto", "Sakage", 2, 2), (7, "Solonariwan Tofusin", "Magi don't need mottoes", "Sethi", 2, 2), (8, "Godking Garoth Ursuul", "I'm a bad guy", "Khalidorian", 2, 2);
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Kylar Stern", "stern undying dier", "human", 2, 2), (6, "Devon Corgi", "I don't have a motto", "Sakage", 2, 2), (7, "Solonariwan Tofusin", "Magi don't need mottoes", "Sethi", 2, 2), (8, "Godking Garoth Ursuul", "I'm a bad guy", "Khalidorian", 2, 2);
 
 INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
 
--- lib/querying.rb
+++ lib/querying.rb
@@ -7,7 +7,7 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1;"
+  "SELECT species, COUNT(*) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
@@ -31,12 +31,12 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books.book_id)
+  "SELECT characters.name, COUNT(*) AS book_count
    FROM characters
    INNER JOIN character_books 
    ON characters.id = character_books.character_id
-   GROUP BY characters.id
-   ORDER BY COUNT(character_books.book_id) DESC, characters.name
+   GROUP BY characters.name
+   ORDER BY COUNT(character_books.book_id) DESC
   ;"
 
 end
