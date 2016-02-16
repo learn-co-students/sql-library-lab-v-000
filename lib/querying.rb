b40a173b80a080df67a1257a22f95ade799cc063
Tom Johns <tommyskies@hotmail.com>
2016-02-16 14:12:41 -0700

    replace INNER JOIN wiht JOIN

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,7 @@ end
 def select_series_title_with_most_human_characters
 	"SELECT series.title
 	FROM series
-	INNER JOIN characters ON series.id = characters.series_id
+	JOIN characters ON series.id = characters.series_id
 	WHERE characters.species='human'
 	GROUP BY series.title
 	ORDER BY COUNT(characters.species) DESC LIMIT 1;"
@@ -40,7 +40,7 @@ end
 def select_character_names_and_number_of_books_they_are_in
 	"SELECT characters.name, COUNT(character_books.book_id)
 	FROM characters
-	INNER JOIN character_books
+	JOIN character_books
 	ON characters.id = character_books.character_id
 	GROUP BY characters.name 
 	ORDER BY COUNT(character_books.book_id) DESC;"
