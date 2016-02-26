f1878a4d895e03a4efc2207ebced6886741afc26
thegands <thegands@gmail.com>
2016-02-25 21:30:43 -0700

    group by ID instead of name

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,9 +15,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN characters ON characters.series_id = series.id WHERE characters.species = \"human\" GROUP BY series.title ORDER BY COUNT() DESC LIMIT 1;"
+  "SELECT series.title FROM series JOIN characters ON characters.series_id = series.id WHERE characters.species = \"human\" GROUP BY series.id ORDER BY COUNT() DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT() FROM characters JOIN character_books ON characters.id = character_books.character_id JOIN books ON books.id = character_books.book_id GROUP BY characters.name ORDER BY COUNT() DESC;"
+  "SELECT characters.name, COUNT() FROM characters JOIN character_books ON characters.id = character_books.character_id JOIN books ON books.id = character_books.book_id GROUP BY characters.id ORDER BY COUNT() DESC, characters.name;"
 end
