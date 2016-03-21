bcd4ed5da76665d24e621ccb12ac9d1984eb597b
glee38 <glee3388@gmail.com>
2016-03-20 23:17:20 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,21 +1,33 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year
+  "SELECT title, year
    FROM books
    WHERE series_id = 1
-   ORDER BY books.year;"
+   ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT characters.name, characters.motto
-   FROM characters
-   WHERE LENGTH(characters.motto) = (SELECT MAX(LENGTH(characters.motto)) FROM characters);"
+  # "SELECT name, motto
+  #  FROM characters
+  #  WHERE LENGTH(motto) = (SELECT MAX(LENGTH(.motto)) FROM characters);"
+
+  # refactored to:
+  "SELECT name, motto 
+   FROM characters 
+   ORDER BY LENGTH(motto) DESC LIMIT 1"
 end
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species)
+  "SELECT species, COUNT(species)
    FROM characters
-   GROUP BY characters.species
-   ORDER BY characters.species DESC LIMIT 1;"
+   GROUP BY species
+   ORDER BY COUNT(species) DESC LIMIT 1;"
+
+   # can also do:
+
+   # "SELECT species, COUNT(*) 
+   #  FROM characters 
+   #  GROUP BY species 
+   #  ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
@@ -24,6 +36,15 @@ def select_name_and_series_subgenres_of_authors
    INNER JOIN series
    ON authors.id = series.author_id
    WHERE subgenres.id = series.subgenre_id;"
+
+   # the following also works:
+
+   # "SELECT authors.name, subgenres.name 
+   #  FROM authors 
+   #  JOIN series 
+   #  ON series.author_id = authors.id 
+   #  JOIN subgenres 
+   #  ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
@@ -34,6 +55,21 @@ def select_series_title_with_most_human_characters
    WHERE characters.species = 'human'
    GROUP BY series.title
    ORDER BY COUNT(series.title) DESC LIMIT 1;"
+
+   # solution on learn:
+   
+   # "SELECT series.title 
+   # FROM series 
+   # JOIN books 
+   # ON books.series_id = series.id 
+   # JOIN character_books 
+   # ON character_books.book_id = books.id 
+   # JOIN characters 
+   # ON character_books.character_id = characters.id 
+   # WHERE characters.species = 'human' 
+   # GROUP BY series.title 
+   # ORDER BY COUNT(*) DESC LIMIT 1"
+
 end
 
 def select_character_names_and_number_of_books_they_are_in
