1be9911b4ff5fa447a59dedcbaab5d4f29bfbd42
gnfisher <me@gnfisher.com>
2016-04-02 20:11:34 -0300

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,41 @@
+INSERT INTO series (title) VALUES ("title 1");
+INSERT INTO series (title) VALUES ("title 2");
+
+INSERT INTO books (title) VALUES ("Any Human Heart");
+INSERT INTO books (title) VALUES ("Brazzaville Beach");
+INSERT INTO books (title) VALUES ("Restless");
+INSERT INTO books (title) VALUES ("A Good Man in Africa");
+INSERT INTO books (title) VALUES ("The Blue Afternoon");
+INSERT INTO books (title) VALUES ("On The Yankee Station");
+
+INSERT INTO characters (name) VALUES ("Adam");
+INSERT INTO characters (name) VALUES ("Brett");
+INSERT INTO characters (name) VALUES ("Charlie");
+INSERT INTO characters (name) VALUES ("Daniel");
+INSERT INTO characters (name) VALUES ("Eric");
+INSERT INTO characters (name) VALUES ("Fredrich");
+INSERT INTO characters (name) VALUES ("George");
+INSERT INTO characters (name) VALUES ("Henry");
+
+INSERT INTO subgenres (name) VALUES ("Noir");
+INSERT INTO subgenres (name) VALUES ("Sea Adventures");
+
+INSERT INTO authors (name) VALUES ("William Boyd");
+INSERT INTO authors (name) VALUES ("James Clavell");
+
+INSERT INTO character_books (book_id, character_id) VALUES (1,1);
+INSERT INTO character_books (book_id, character_id) VALUES (1,2);
+INSERT INTO character_books (book_id, character_id) VALUES (1,3);
+INSERT INTO character_books (book_id, character_id) VALUES (2,4);
+INSERT INTO character_books (book_id, character_id) VALUES (2,5);
+INSERT INTO character_books (book_id, character_id) VALUES (2,6);
+INSERT INTO character_books (book_id, character_id) VALUES (3,9);
+INSERT INTO character_books (book_id, character_id) VALUES (3,8);
+INSERT INTO character_books (book_id, character_id) VALUES (3,7);
+INSERT INTO character_books (book_id, character_id) VALUES (4,6);
+INSERT INTO character_books (book_id, character_id) VALUES (4,5);
+INSERT INTO character_books (book_id, character_id) VALUES (4,4);
+INSERT INTO character_books (book_id, character_id) VALUES (5,3);
+INSERT INTO character_books (book_id, character_id) VALUES (5,2);
+INSERT INTO character_books (book_id, character_id) VALUES (6,1);
+INSERT INTO character_books (book_id, character_id) VALUES (6,2);
