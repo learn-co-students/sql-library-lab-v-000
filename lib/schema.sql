90318654ffb024b5a5e756aa0dd18f09479059c8
maireadmurphy <mairead.aw.murphy@gmail.com>
2016-05-16 13:13:46 -0400

    Done.

---
+++ lib/schema.sql
@@ -0,0 +1,44 @@
+
+#Series: title, author name, sub-genre
+#Sub-Genre: name
+#Authors: name
+#Books: title, year, belong to series
+#Characters: name, motto, species, belong to author, belong to series
+CREATE TABLE series (
+id INTEGER PRIMARY KEY,
+title TEXT,
+author_id INTEGER,
+subgenre_id INTEGER
+);
+
+CREATE TABLE subgenres (
+id INTEGER PRIMARY KEY,
+name TEXT
+);
+
+CREATE TABLE authors (
+id INTEGER PRIMARY KEY,
+name TEXT
+);
+
+CREATE TABLE books (
+id INTEGER PRIMARY KEY,
+title TEXT,
+year TEXT,
+series_id INTEGER
+);
+
+CREATE TABLE characters (
+id INTEGER PRIMARY KEY,
+name TEXT,
+species TEXT,
+motto TEXT,
+series_id INTEGER,
+author_id INTEGER
+);
+
+CREATE TABLE character_books (
+id INTEGER PRIMARY KEY,
+character_id INTEGER,
+book_id INTEGER
+);
