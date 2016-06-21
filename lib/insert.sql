e45b3199251e351fb891efbb592581a9e4157c2f
Jason <jasonkwong11@gmail.com>
2016-06-20 22:56:40 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,114 @@
+INSERT INTO series (title, author_id, subgenre_id)
+  VALUES ("Harry Potter", 1, 2);
+
+INSERT INTO series (title, author_id, subgenre_id)
+  VALUES ("Lord of the Rings", 2, 2);
+
+INSERT INTO books (title, year, series_id)
+  VALUES ("Deathly Hallows", 2011, 1);
+
+INSERT INTO books (title, year, series_id)
+  VALUES ("Goblet of Fire", 2006, 1);
+
+INSERT INTO books (title, year, series_id)
+  VALUES ("Return of the King", 2008, 2);
+
+INSERT INTO books (title, year, series_id)
+  VALUES ("Fellowship of the Ring", 2007, 2);
+
+INSERT INTO books (title, year, series_id)
+  VALUES ("Charlotte's Web", 1976, 3);
+
+INSERT INTO books (title, year, series_id)
+  VALUES ("To Kill a Mockingbird", 1966, 4);
+
+INSERT INTO characters (name, species, motto, series_id, author_id)
+  VALUES ("Legolas", "Elf", "And my bow.", 2, 2);
+
+
+INSERT INTO characters (name, species, motto, series_id, author_id)
+  VALUES ("Legolas", "Elf", "And my bow.", 2, 2);
+
+
+INSERT INTO characters (name, species, motto, series_id, author_id)
+  VALUES ("Legolas", "Elf", "And my bow.", 2, 2);
+
+
+INSERT INTO characters (name, species, motto, series_id, author_id)
+  VALUES ("Legolas", "Elf", "And my bow.", 2, 2);
+
+
+INSERT INTO characters (name, species, motto, series_id, author_id)
+  VALUES ("Legolas", "Elf", "And my bow.", 2, 2);
+
+
+INSERT INTO characters (name, species, motto, series_id, author_id)
+  VALUES ("Legolas", "Elf", "And my bow.", 2, 2);
+
+
+INSERT INTO characters (name, species, motto, series_id, author_id)
+  VALUES ("Legolas", "Elf", "And my bow.", 2, 2);
+
+
+INSERT INTO characters (name, species, motto, series_id, author_id)
+  VALUES ("Legolas", "Elf", "And my bow.", 2, 2);
+
+INSERT INTO subgenres (name)
+  VALUES ("Fantasy");
+
+INSERT INTO subgenres (name)
+  VALUES ("Historical Fiction");
+
+INSERT INTO authors (name)
+  VALUES ("JK Rowling");
+
+INSERT INTO authors (name)
+  VALUES ("JRR Tolkien");
+
+INSERT INTO character_books (book_id, character_id)
+  VALUES (5,2);
+
+INSERT INTO character_books (book_id, character_id)
+  VALUES (1,1);
+
+INSERT INTO character_books (book_id, character_id)
+  VALUES (1,3);
+
+INSERT INTO character_books (book_id, character_id)
+  VALUES (1,4);
+
+INSERT INTO character_books (book_id, character_id)
+  VALUES (1,4);
+
+INSERT INTO character_books (book_id, character_id)
+  VALUES (1,4);
+
+INSERT INTO character_books (book_id, character_id)
+  VALUES (1,4);
+
+INSERT INTO character_books (book_id, character_id)
+  VALUES (1,4);
+
+INSERT INTO character_books (book_id, character_id)
+  VALUES (1,4);
+
+INSERT INTO character_books (book_id, character_id)
+  VALUES (1,4);
+
+INSERT INTO character_books (book_id, character_id)
+  VALUES (1,4);
+
+INSERT INTO character_books (book_id, character_id)
+  VALUES (1,4);
+
+INSERT INTO character_books (book_id, character_id)
+  VALUES (1,4);
+
+INSERT INTO character_books (book_id, character_id)
+  VALUES (1,4);
+
+INSERT INTO character_books (book_id, character_id)
+  VALUES (1,4);
+
+INSERT INTO character_books (book_id, character_id)
+  VALUES (1,4);
