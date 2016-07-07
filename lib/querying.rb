5e811f10102145c286b441c02c16cbaa11480cff
Bob Habig <rfhab2@gmail.com>
2016-07-07 22:56:59 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,21 +39,10 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(*)
+  "SELECT characters.name, COUNT(character_books.book_id)
    FROM characters
    JOIN character_books
    ON characters.id = character_books.character_id
    GROUP BY characters.name
-   ORDER BY COUNT(*) DESC;"
+   ORDER BY COUNT(book_id) DESC;"
 end
-
-#def select_character_names_and_number_of_books_they_are_in ???
-#  "SELECT characters.name, COUNT(character_books.book_id)
-#   FROM characters
-#   JOIN character_books
-#   ON characters.id = character_books.character_id
-#   JOIN books
-#   ON character_books.book_id = books.id
-#   GROUP BY books.id
-#   ORDER BY COUNT(book_id) DESC;"
-#end
