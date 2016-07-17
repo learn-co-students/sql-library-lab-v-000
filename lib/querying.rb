54969e7d86adfb2c1e3db15878ee345828811cb9
Adam Taitano <adam.taitano@gmail.com>
2016-07-17 00:00:59 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,17 +1,23 @@
 def select_books_titles_and_years_in_first_series_order_by_year
+  "SELECT books.title, books.year FROM books INNER JOIN series WHERE books.series_id = 1 ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
+  "SELECT characters.name, characters.motto FROM characters ORDER BY characters.motto LIMIT 1;"
 end
 
 def select_value_and_count_of_most_prolific_species
+  "SELECT characters.species, COUNT(characters.species) FROM characters ORDER BY characters.species LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
+  "SELECT authors.name, subgenres.name FROM authors INNER JOIN subgenres WHERE "
 end
 
 def select_series_title_with_most_human_characters
+  "SELECT series.title FROM series INNER JOIN characters WHERE characters.species = 'human';"
 end
 
 def select_character_names_and_number_of_books_they_are_in
+  "SELECT characters.name, COUNT(books)"
 end
---
+++ lib/update.sql
@@ -0,0 +1,1 @@
+UPDATE characters SET characters.species = "Martian" WHERE characters.name = "Character Four" ORDER BY characters.name DESC LIMIT 1
