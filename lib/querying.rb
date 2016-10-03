a390332f768e3e1f377c51670caf31a490a95adb
Elyse Klova <elyse.klova@gmail.com>
2016-10-03 19:40:47 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,7 @@ def select_series_title_with_most_human_characters
   FROM series
   INNER JOIN characters
   ON series.id = characters.series_id
-  GROUP BY series.title HAVING COUNT(characters.species = 'human');"
+  GROUP BY characters.series_id HAVING COUNT(characters.species) WHERE characters.species = 'human';"
 end
 
 def select_character_names_and_number_of_books_they_are_in
