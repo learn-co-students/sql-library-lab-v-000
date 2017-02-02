12f823a62974777f0564487c4c364160ab430f8e
Juan Rivera <juanriv45@gmail.com>
2017-02-02 02:51:12 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,42 @@
+INSERT INTO series VALUES(1,"Hunger Games",1,1);
+INSERT INTO series VALUES(2,"Lord Of The Rings",2,2);
+
+INSERT INTO subgenres VALUES(1,"Fiction");
+INSERT INTO subgenres VALUES(2,"Fantasy");
+
+INSERT INTO authors VALUES(1,"Hungerman");
+INSERT INTO authors VALUES(2,"Tolkien");
+
+INSERT INTO books VALUES(1,"Eat 1","1991",1);
+INSERT INTO books VALUES(2,"Eat 2","1992",1);
+INSERT INTO books VALUES(3,"Eat 3","1993",1);
+INSERT INTO books VALUES(4,"Ring 1","1975",2);
+INSERT INTO books VALUES(5,"Ring 2","1976",2);
+INSERT INTO books VALUES(6,"Ring 3","1977",2);
+
+INSERT INTO characters VALUES(1,"Mike","Get it","human",1,1);
+INSERT INTO characters VALUES(2,"Joe","Make it happen","human",1,1);
+INSERT INTO characters VALUES(3,"Rako","Run or Run","Lion",1,1);
+INSERT INTO characters VALUES(4,"Pope","Can't stop, won't stop","human",1,1);
+INSERT INTO characters VALUES(5,"Tony","Shock to your system","elf",2,2);
+INSERT INTO characters VALUES(6,"Matthew","Rock and roll","wolf",2,2);
+INSERT INTO characters VALUES(7,"Shark","Bite me","fish",2,2);
+INSERT INTO characters VALUES(8,"Rojo","Fire in the hole","monkey",2,2);
+
+INSERT INTO character_books VALUES(1,1,1);
+INSERT INTO character_books VALUES(2,1,2);
+INSERT INTO character_books VALUES(3,2,1);
+INSERT INTO character_books VALUES(4,2,2);
+INSERT INTO character_books VALUES(5,3,1);
+INSERT INTO character_books VALUES(6,3,2);
+INSERT INTO character_books VALUES(7,3,3);
+INSERT INTO character_books VALUES(8,2,4);
+INSERT INTO character_books VALUES(9,4,4);
+INSERT INTO character_books VALUES(10,4,5);
+INSERT INTO character_books VALUES(11,5,4);
+INSERT INTO character_books VALUES(12,5,5);
+INSERT INTO character_books VALUES(13,6,4);
+INSERT INTO character_books VALUES(14,6,5);
+INSERT INTO character_books VALUES(15,5,8);
+INSERT INTO character_books VALUES(16,6,7);
+
