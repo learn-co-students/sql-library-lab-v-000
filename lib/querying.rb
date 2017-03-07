c1027be0abc88bbfea177bfbac081c85c4e474a7
bentonwong <bentonwong@gmail.com>
2017-03-06 20:02:27 -0800

    updated queries

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,13 +1,21 @@
 def select_books_titles_and_years_in_first_series_order_by_year
+  "SELECT books.title, books.year FROM books WHERE series_id = 1 ORDER BY year ASC;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
+  "SELECT characters.name, characters.motto FROM characters WHERE LENGTH(characters.motto) = (SELECT MAX(LENGTH(characters.motto)) FROM characters);"
 end
 
 def select_value_and_count_of_most_prolific_species
+  "SELECT species, COUNT(species) FROM characters GROUP BY species ORDER BY species DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
+  "SELECT authors.name, subgenres.name
+  FROM authors
+  INNER JOIN series
+  ON authors.id = series.author_id
+  JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
