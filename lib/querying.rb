23a3407929b6cd483c61e8fb15411483f3de9a89
Mason Ellwood <masonellwood@gmail.com>
2017-03-31 06:52:12 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,17 +1,53 @@
 def select_books_titles_and_years_in_first_series_order_by_year
+  "
+  SELECT title, year FROM books ORDER BY year LIMIT 3
+  "
 end
 
 def select_name_and_motto_of_char_with_longest_motto
+  "
+  SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1
+  "
 end
 
 def select_value_and_count_of_most_prolific_species
+  "
+  SELECT species, COUNT(species) FROM characters GROUP BY species ORDER BY species desc LIMIT 1
+  "
 end
 
 def select_name_and_series_subgenres_of_authors
+  "
+  SELECT authors.name, subgenres.name
+  FROM authors
+  JOIN series
+  ON authors.id = series.author_id
+  JOIN subgenres
+  ON subgenres.id = series.subgenre_id
+  "
 end
 
 def select_series_title_with_most_human_characters
+  "
+  SELECT series.title
+  FROM series
+  JOIN characters
+  ON series.id = characters.series_id
+  WHERE characters.species = 'human'
+  ORDER BY characters.species ASC
+  LIMIT 1
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
+  "
+  SELECT characters.name, count(*)
+  FROM characters
+  JOIN character_books
+  ON characters.id = character_books.character_id
+  JOIN books
+  ON character_books.book_id = books.id
+  GROUP BY characters.name
+  ORDER BY COUNT(*) DESC
+  "
 end
