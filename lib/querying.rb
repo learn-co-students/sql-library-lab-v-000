843a031544d160b26f1b82ae6c60e299bbaed15a
Aditi Green <asaki444@gmail.com>
2017-05-03 04:11:14 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,10 +1,17 @@
 def select_books_titles_and_years_in_first_series_order_by_year
+  "SELECT books.title, books.year FROM books
+   JOIN series ON books.series_id = series.id
+   WHERE series.id = 1
+   ORDER BY books.year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
+  "SELECT characters.name, characters.motto FROM characters
+   ORDER BY LENGTH(characters.motto) DESC LIMIT 1"
 end
 
 def select_value_and_count_of_most_prolific_species
+  "SELECT characters.species COUNT(characters.species) FROM characters"
 end
 
 def select_name_and_series_subgenres_of_authors
