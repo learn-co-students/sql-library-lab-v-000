30c104c4a7b64f7768fb4aa9c8da0921726b7f29
Will-Sauer <will@heueso.com>
2017-06-01 14:34:13 -0700

    all schema specs passing

---
+++ lib/schema.sql
@@ -0,0 +1,39 @@
+create table characters (
+            id INTEGER PRIMARY KEY,
+            name TEXT, 
+            motto TEXT, 
+            species TEXT,
+            author_id INTEGER,
+            series_id INTEGER
+            );
+
+create table books (
+            id INTEGER PRIMARY KEY,
+            title TEXT,
+            year INTEGER,
+            series_id INTEGER
+            );
+
+create table series (
+            id INTEGER PRIMARY KEY,
+            title TEXT,
+            author_id INTEGER,
+            subgenre_id INTEGER
+            );
+
+create table authors (
+            id INTEGER PRIMARY KEY,
+            name TEXT
+            );
+
+create table subgenres (
+            id INTEGER PRIMARY KEY,
+            name TEXT
+            );
+
+
+create table character_books (
+                            id INTEGER PRIMARY KEY,
+                            character_id INTEGER,
+                            book_id INTEGER
+                            );
