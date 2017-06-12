c02bae6b35e3d46e1ce6325739d53e9c5c8e8f4d
Yevgeniy Skroznikov <ohnap@hotmail.com>
2017-06-12 19:01:15 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT characters.name, characters.motto FROM characters ORDER BY characters.motto ASC LIMIT 1;"
+  "SELECT characters.name, characters.motto FROM characters ORDER BY LENGTH(characters.motto) DESC LIMIT 1;"
 end
 
 def select_value_and_count_of_most_prolific_species
@@ -15,9 +15,10 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series INNER JOIN characters ON series.id = characters.series_id WHERE characters.species = \"human\" ORDER BY characters.species DESC LIMIT 1;"
+  "SELECT series.title FROM series JOIN books ON books.series_id = series.id JOIN character_books ON character_books.book_id = books.id JOIN characters ON character_books.character_id = characters.id WHERE characters.species = \"human\" GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books.character_id) FROM characters INNER JOIN character_books ON character_books.character_id = characters.id GROUP BY characters.name ORDER BY COUNT(character_books.character_id) DESC;"
+  #"SELECT characters.name, COUNT(character_books.character_id) FROM characters INNER JOIN character_books ON character_books.character_id = characters.id GROUP BY characters.name ORDER BY COUNT(character_books.character_id) DESC;"
+  "SELECT characters.name, COUNT(*) as book_count from character_books JOIN characters ON character_books.character_id = characters.id GROUP BY characters.name ORDER BY book_count DESC"
 end
