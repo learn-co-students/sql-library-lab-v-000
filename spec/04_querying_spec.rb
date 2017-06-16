6889d685355292b856c1babe208ed2f9761e107a
Scott Jurkowski <scottjurkowski@Crash-Cart.local>
2017-06-16 16:05:10 -0500

    Done.

--- spec/04_querying_spec.rb
+++ spec/04_querying_spec.rb
@@ -11,15 +11,15 @@ describe 'querying' do
   end
 
   it 'returns the name and motto of the character with the longest motto' do
-    expect(@db.execute(select_name_and_motto_of_char_with_longest_motto)).to eq([["Tyrion Lanister", "A Lanister always pays is debts"]])
+    expect(@db.execute(select_name_and_motto_of_char_with_longest_motto)).to eq([["Arthur Dent", "I never could get the hang of wednesdays"]])
   end
 
   it 'determines the most prolific species of characters and return its value and count' do
-    expect(@db.execute(select_value_and_count_of_most_prolific_species)).to eq([["human", 4]])
+    expect(@db.execute(select_value_and_count_of_most_prolific_species)).to eq([["human", 5]])
   end
 
   it "selects the authors names and their series' subgenres" do
-    expect(@db.execute(select_name_and_series_subgenres_of_authors)).to eq([["George R. R. Martin", "medieval"], ["Second Author", "space opera"]])
+    expect(@db.execute(select_name_and_series_subgenres_of_authors)).to eq([["George R. R. Martin", "medieval"], ["Douglas Adams", "space python"]])
   end
 
   it 'selects the series title with the most characters that are the species "human"' do
@@ -27,6 +27,6 @@ describe 'querying' do
   end
 
   it 'selects all of the character names and their number of books they have appeared in, in descending order' do
-    expect(@db.execute(select_character_names_and_number_of_books_they_are_in)).to eq([["Character Three",3], ["Character Two", 3],["Daenerys Targaryen", 3], ["Tyrion Lanister", 3], ["Character Four", 1], ["Character One", 1], ["Eddard Stark", 1], ["Lady", 1]])
+    expect(@db.execute(select_character_names_and_number_of_books_they_are_in)).to eq([["Daenerys Targaryen", 3], ["Ford Prefect", 3],["Tyrion Lanister", 3], ["Zaphod Beeblebrox",3], ["Arthur Dent", 1], ["Eddard Stark", 1], ["Lady", 1], ["Trisha MacMillan", 1]])
   end
 end
