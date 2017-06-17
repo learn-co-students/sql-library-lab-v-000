1c333d107b027e44dac7f668d46e8408bf90d582
Margarita Khodzka <mkhodzka@gmail.com>
2017-06-17 19:45:16 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,23 +1,23 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year
+  "SELECT title, year
    FROM books
-   INNER JOIN series
-   ON books.series_id = series.id
-   WHERE series.id = 1
-   ORDER BY books.year;"
+   WHERE series_id = 1
+   ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto
    FROM characters
-   ORDER BY LENGTH(motto) DESC LIMIT 1;"
+   ORDER BY LENGTH(motto) DESC
+   LIMIT 1;"
 end
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) AS count
+  "SELECT species, COUNT(species)
    FROM characters
    GROUP BY species
-   ORDER BY count DESC LIMIT 1;"
+   ORDER BY COUNT(species) DESC
+   LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
@@ -34,16 +34,14 @@ def select_series_title_with_most_human_characters
    ON series.id = characters.series_id
    WHERE characters.species = 'human'
    GROUP BY series.title
-   ORDER BY COUNT(characters.species) DESC LIMIT 1;"
+   LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books.character_id) AS count
+  "SELECT characters.name, COUNT(character_books.character_id)
    FROM characters
    INNER JOIN character_books
    ON characters.id = character_books.character_id
-   JOIN books
-   ON character_books.book_id = books.id
    GROUP BY characters.name
-   ORDER BY count DESC;"
+   ORDER BY COUNT(character_books.character_id) DESC;"
 end
