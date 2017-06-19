ceefdd261ace2eac7d3c5fcd31f3c1a8577f7c1a
hyoyou <heatherhyou@gmail.com>
2017-06-19 12:04:20 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
-  INNER JOIN series ON series.author_id = authors.id
+  INNER JOIN series ON authors.id = series.author_id
   INNER JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
