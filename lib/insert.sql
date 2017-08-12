ebf2fa478a1f5cacb86aaf359a6ad34e0450c45f
jonathan <jonathanyi81@gmail.com>
2017-08-11 21:52:53 -0400

    complete the inserts for 2 series

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,38 +1,46 @@
 INSERT INTO series (id, title, author_id, subgenre_id)
-VALUES (1, "The Kingkiller Chronicle", 1, 1), (2, "Second Series", 2, 2);
+VALUES (1, "The Kingkiller Chronicle", 1, 1), (2, "Mitch Rapp", 2, 2);
 
 INSERT INTO authors (id, name)
-VALUES (1, "Patrick Rothfuss"), (2, "Second Author");
+VALUES (1, "Patrick Rothfuss"), (2, "Vince Flynn");
 
 INSERT INTO subgenres (id, name)
-VALUES (1, "Heroic fantasy"), (2, "space opera");
+VALUES (1, "Heroic Fantasy"), (2, "Political Thriller");
 
 INSERT INTO books (id, title, year, series_id)
 VALUES
-  (1, "The Name of the Wind", 2007, 1), (2, "The Wise Man's Fear", 2011, 2), (3, "The Doors of Stone", 999, 3),
-  (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
+  (1, "The Name of the Wind", 2007, 1), (2, "The Wise Man's Fear", 2011, 2), (3, "The Doors of Stone", 9999, 3),
+  (4, "American Assassin", 2010, 1), (5, "Kill Shot", 2012, 2), (6, "Transfer of Power", 1999, 3);
 
 INSERT INTO characters (id, name, motto, species, author_id, series_id)
 VALUES
-  (1, "Character One", "motto one", "cylon", 2, 2),
-  (6, "Character Two", "motto two", "human", 2, 2),
-  (7, "Character Three", "motto three", "cylon", 2, 2),
-  (8, "Character Four", "motto four", "cylon", 2, 2);
+  (1, "Kvothe", "Namer", "human", 1, 1),
+  (2, "Denna", "Always on the move", "human", 1, 1),
+  (3, "The Cthaeh", "I am. I see. I know. At times I speak", "unkown", 1, 1),
+  (4, "Tempi", "", "human", 1, 1);
 
 INSERT INTO characters (id, name, motto, species, author_id, series_id)
 VALUES
-  (2, "Character One", "motto one", "cylon", 2, 2),
-  (6, "Character Two", "motto two", "human", 2, 2),
-  (7, "Character Three", "motto three", "cylon", 2, 2),
-  (8, "Character Four", "motto four", "cylon", 2, 2);
+  (5, "Mitch Rapp", "Secreat operative for CIA", "human", 2, 2),
+  (6, "Irene Kennedy", "Patriot", "human", 2, 2),
+  (7, "Stan Hurley", "Carnky old school spy", "human", 2, 2),
+  (8, "Doctor Louis", "Pysch guy", "human", 2, 2);
 
 INSERT INTO character_books (id, book_id, character_id)
 VALUES
-  (1, 4, 5),
-  (2, 4, 6),
-  (3, 5, 6),
-  (4, 6, 6),
-  (5, 4, 7),
-  (6, 5, 7),
-  (7, 6, 7),
-  (8, 4, 8);
+  (1, 1, 1),
+  (2, 2, 1),
+  (3, 3, 1),
+  (4, 1, 2),
+  (5, 2, 2),
+  (6, 3, 2),
+  (7, 2, 3),
+  (8, 2, 4),
+  (9, 4, 5),
+  (10, 5, 5),
+  (11, 6, 5),
+  (12, 4, 6),
+  (13, 5, 6),
+  (14, 6, 6),
+  (15, 4, 7),
+  (16, 4, 8);
