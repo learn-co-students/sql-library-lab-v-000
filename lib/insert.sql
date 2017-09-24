2abfc73d34f468a1c1407aa28fad87525cdbbf52
Johan Esteves <jsesteves91@gmail.com>
2017-09-24 18:01:55 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO series(title, author_id, series_id) VALUES ("series1", 1, 2);
+INSERT INTO series(title, author_id, series_id) VALUES ("series2", 2, 1);
+
+INSERT INTO subgenres(name) VALUES ("subgenre1");
+INSERT INTO subgenres(name) VALUES ("subgenre2");
+
+INSERT INTO authors(name) VALUES ("author1");
+INSERT INTO authors(name) VALUES ("author2");
+
+INSERT INTO books(title, author_id, series_id) VALUES ("book1", 1999, 1)
+INSERT INTO books(title, author_id, series_id) VALUES ("book2", 2000, 1)
+INSERT INTO books(title, author_id, series_id) VALUES ("book3", 2001, 2)
+
+INSERT INTO series(title, author_id, series_id) VALUES ("series1", 1, 2)
