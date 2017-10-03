c9bbc09fadc499d0270ccc343f01d6c9263145ae
Chris Rudolph <chrisjames.r@gmail.com>
2017-10-03 23:29:14 +0000

    complete all sql queries

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,17 +14,37 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species)
+   FROM characters
+   GROUP BY characters.species
+   ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+    FROM authors
+    INNER JOIN series
+    ON authors.id = series.author_id
+    JOIN subgenres
+    ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+    FROM series
+    LEFT OUTER JOIN characters
+    ON series.id = characters.series_id
+    GROUP BY characters.species
+    HAVING MAX(characters.species = 'human');"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(books.id)
+    FROM characters
+    INNER JOIN character_books
+    ON characters.id = character_books.character_id
+    JOIN books
+    ON character_books.book_id = books.id
+    GROUP BY characters.name
+    ORDER BY COUNT(Books.id) DESC;"
 end
