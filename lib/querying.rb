649a433811a98e8630fbdd3b3b0f6b79e4e208d7
github username <github email address>
2017-10-04 15:34:27 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -9,7 +9,7 @@ end
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto
   FROM characters
-  ORDER BY length(characters.motto) DESC
+  ORDER BY LENGTH(characters.motto) DESC
   LIMIT 1
   ;"
 end
@@ -37,6 +37,17 @@ def select_series_title_with_most_human_characters
   ORDER BY characters.species DESC
   LIMIT 1
   ;"
+
+  #SOLUTION
+  # "SELECT series.title
+  #  FROM series
+  #  JOIN books ON books.series_id = series.id
+  #  JOIN character_books ON character_books.book_id = books.id
+  #  JOIN characters ON character_books.character_id = characters.id
+  #  WHERE characters.species = 'human'
+  #  GROUP BY series.title
+  #  ORDER BY COUNT(*) DESC
+  #  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
