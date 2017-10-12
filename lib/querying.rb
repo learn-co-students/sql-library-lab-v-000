0e49c078174d89d66f08ff92f50e4518664f0d0a
Melissa Amaya <amaya.melissa@gmail.com>
2017-10-12 10:59:33 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,23 +16,36 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species) AS count
+  "SELECT characters.species, COUNT(characters.species) AS species_count
   FROM characters
-  ORDER BY count
+  GROUP BY characters.species
+  ORDER BY species_count DESC
   LIMIT 1";
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, series.subgenre_id
+  "SELECT authors.name, subgenres.name
   FROM authors
-  JOIN ON authors.id = subgenres.id
-    JOIN ON subgenres.id = series.subgenre_id";
+  JOIN series ON authors.id = series.author_id
+  JOIN subgenres ON series.subgenre_id = subgenres.id";
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM series
+  JOIN characters ON series.id = characters.series_id
+  WHERE characters.species = 'human'
+  GROUP BY series.title
+  ORDER BY  COUNT(characters.species) DESC
+  LIMIT 1;"
+
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(character_books.id) AS book_count
+  FROM characters
+  JOIN character_books ON characters.id = character_books.character_id
+  GROUP BY characters.name
+  ORDER BY book_count DESC
+  ";
 end
