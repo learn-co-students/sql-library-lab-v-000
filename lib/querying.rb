0e526acb40e808acd4cdf2933658f07801c6a49f
Nicholas Halloran <nmhalloran@hotmail.com>
2017-10-18 15:25:44 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,9 +23,9 @@ end
 def select_series_title_with_most_human_characters
   "SELECT title FROM Series
   INNER JOIN Characters ON characters.series_id = series.id
+  WHERE characters.species = 'human'
   GROUP BY title
-  ORDER BY COUNT(characters.species = 'human') DESC LIMIT 1;"
-  binding.pry
+  ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
