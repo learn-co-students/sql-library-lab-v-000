52ada8a0d96d6c38732b2dc04f210ea0caba6424
Nicholas Halloran <nmhalloran@hotmail.com>
2017-10-18 15:09:00 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,6 +25,7 @@ def select_series_title_with_most_human_characters
   INNER JOIN Characters ON characters.series_id = series.id
   GROUP BY title
   ORDER BY COUNT(characters.species = 'human') DESC LIMIT 1;"
+  binding.pry
 end
 
 def select_character_names_and_number_of_books_they_are_in
