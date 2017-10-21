a06cfdabb93fcecc8ef88c9d42a67b0f2697b79e
Denis Bondin <dsbondin@gmail.com>
2017-10-21 20:04:29 +0000

    ...

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO series(title, author_id, subgenre_id) VALUES
+("Amelia Bedelia", 1, 1),
+("Harry Potter", 2, 2);
+
+INSERT INTO books(title, year, series_id) VALUES
+("Thank You, Amelia Bedelia", 1964, 1),
+("Amelia Bedelia and the Surprise Shower", 1966, 1),
+("Come Back, Amelia Bedelia", 1971, 1),
+("Harry Potter and the Philosopher\'s Stone", )
