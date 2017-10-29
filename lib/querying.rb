98caa44f63aff37d5d231510062234e1625961cb
Yehuda Makarov <yehudamakarov@gmail.com>
2017-10-29 09:55:02 -0400

    ReCommiting?

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "select books.title, books.year from books where books.series_id = 1 order by books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "select characters.species, count(characters.species) from characters group by species order by count(characters.species) desc limit 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
