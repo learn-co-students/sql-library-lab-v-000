11ce3bd1ba8d716c2fce61e6de6cc5ce5ce1522f
Augusto Granitto <augustogranitto@gmail.com>
2017-11-13 01:17:47 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO series (title) VALUES ('test'), ('test');
+INSERT INTO books (title) VALUES ('test'), ('test'), ('test'), ('test'), ('test'), ('test');
+INSERT INTO characters (name) VALUES ('test'), ('test'), ('test'), ('test'), ('test'), ('test'), ('test'), ('test');
+INSERT INTO subgenres (name) VALUES ('test'), ('test');
+INSERT INTO authors (name) VALUES ('test'), ('test');
+INSERT INTO character_books (book_id) VALUES ('test'), ('test'), ('test'), ('test'), ('test'), ('test'), ('test'), ('test'), ('test'), ('test'), ('test'), ('test'), ('test'), ('test'), ('test'), ('test');
