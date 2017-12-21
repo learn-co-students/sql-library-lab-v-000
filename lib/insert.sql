fe6b9b396d2ee3572ccf36087d9b468ec7975549
Joshua Needham <joshuabneedham@gmail.com>
2017-12-21 09:35:16 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,41 @@
+INSERT INTO series (id) VALUES (1);
+INSERT INTO series (id) VALUES (2);
+
+INSERT INTO books (id) VALUES (1);
+INSERT INTO books (id) VALUES (2);
+INSERT INTO books (id) VALUES (3);
+INSERT INTO books (id) VALUES (4);
+INSERT INTO books (id) VALUES (5);
+INSERT INTO books (id) VALUES (6);
+
+INSERT INTO characters (id) VALUES (1);
+INSERT INTO characters (id) VALUES (2);
+INSERT INTO characters (id) VALUES (3);
+INSERT INTO characters (id) VALUES (4);
+INSERT INTO characters (id) VALUES (5);
+INSERT INTO characters (id) VALUES (6);
+INSERT INTO characters (id) VALUES (7);
+INSERT INTO characters (id) VALUES (8);
+
+INSERT INTO subgenres (id) VALUES (1);
+INSERT INTO subgenres (id) VALUES (2);
+
+INSERT INTO authors (id) VALUES (1);
+INSERT INTO authors (id) VALUES (2);
+
+INSERT INTO character_books (id) VALUES (1);
+INSERT INTO character_books (id) VALUES (2);
+INSERT INTO character_books (id) VALUES (3);
+INSERT INTO character_books (id) VALUES (4);
+INSERT INTO character_books (id) VALUES (5);
+INSERT INTO character_books (id) VALUES (6);
+INSERT INTO character_books (id) VALUES (7);
+INSERT INTO character_books (id) VALUES (8);
+INSERT INTO character_books (id) VALUES (9);
+INSERT INTO character_books (id) VALUES (10);
+INSERT INTO character_books (id) VALUES (11);
+INSERT INTO character_books (id) VALUES (12);
+INSERT INTO character_books (id) VALUES (13);
+INSERT INTO character_books (id) VALUES (14);
+INSERT INTO character_books (id) VALUES (15);
+INSERT INTO character_books (id) VALUES (16);
