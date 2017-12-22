ec25878ca3ededc855665dfd96017220a8b9709b
github username <github email address>
2017-12-22 11:02:26 -0600

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,24 +4,20 @@ end
 
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto, LENGTH(motto) AS char_length FROM characters GROUP BY motto HAVING MAX(char_length);"
+  "SELECT name, motto FROM characters GROUP BY motto HAVING MAX(LENGTH(motto)) LIMIT 1;"
 end
 
-# LOOK UP SQL character size length
-# WHERE LENGTH(pub_name)>=20;
+def select_value_and_count_of_most_prolific_species
+  "SELECT species, COUNT(species) AS species_count FROM characters GROUP BY species ORDER BY species_count DESC LIMIT 1;"
+end
 
+def select_name_and_series_subgenres_of_authors
+  "SELECT authors.name, subgenres.name FROM authors, subgenres JOIN series ON subgenres.id = series.subgenre_id WHERE authors.id = series.author_id;"
+end
 
-# def select_value_and_count_of_most_prolific_species
-#   "SELECT COUNT (species), value FROM characters LIMIT 1;"
-# end
-
-# def select_name_and_series_subgenres_of_authors
-#   "SELECT subgenre_id FROM series AND author_id FROM series INNER JOIN authors ON authors.id = series.author_id AND INNER JOIN subgenres ON subgenres.id = series.subgenre_id;"
-# end
-
-# def select_series_title_with_most_human_characters
-#   "SELECT title FROM series INNER JOIN series ON series.id = characters.series_id ORDER BY species WHERE character.species = "human" LIMIT 1;"
-# end
+def select_series_title_with_most_human_characters
+  "SELECT title, COUNT(species = 'human') AS human_count FROM series JOIN characters ON series.id = characters.series_id GROUP BY series.title ORDER BY human_count DESC LIMIT 1;"
+end
 
 # def select_character_names_and_number_of_books_they_are_in
 #   "SELECT name FROM characters INNER JOIN books WHERE books.series_id = characters.series_id AND INNER JOIN series WHERE books.series_id = series.id COUNT (books.name) ORDER BY books.name.count, books.name;"
