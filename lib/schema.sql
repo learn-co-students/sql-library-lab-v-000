1481445ba9fe0726cec4a78fe60ebc94f58c8f93
Elle Faraday <faraday.elle@gmail.com>
2017-12-31 20:12:51 +0000

    Done.

---
+++ lib/schema.sql
@@ -0,0 +1,48 @@
+/*1. All tables must have a `PRIMARY KEY` on the id
+2. `Series` have a title and belong to an author and a sub-genre
+3. A `Sub-Genre` has a name
+4. `Authors` have a name
+5. `Books` have a title and year and belong to a series
+6. `Characters` have a name, motto, and species and belong to an author and a series
+7. `Books` have many characters and characters are in many books in a series.
+How do we accomplish this complex association? With a join table between Characters and Books.
+This join table (let's call it character_books) will just have -in addition to its primary key- two foreign key columns for the character and book ids.
+Each row in this join table acts as a relation between a book and a character.*/
+
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  author INTEGER,
+  sub-genre INTEGER
+);
+
+CREATE TABLE sub_genres (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY,
+  character_id INTEGER,
+  book_id INTEGER
+);
