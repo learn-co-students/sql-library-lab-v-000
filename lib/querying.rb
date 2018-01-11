c1616325934f81cb6e752b849f950cf2f62ac542
Brittany Morris <brittmmendez@gmail.com>
2018-01-11 14:05:58 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,13 +18,28 @@ ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name,subgenres.name FROM authors
+JOIN series
+ON series.author_id=authors.id
+JOIN subgenres
+ON series.subgenre_id=subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series
+JOIN characters
+ON series.author_id=characters.author_id
+WHERE characters.species='human'
+GROUP BY  series.title
+ORDER BY COUNT(*) DESC LIMIT 1;
+
+"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(*) FROM characters
+JOIN character_books
+ON character_books.character_id=characters.id
+GROUP BY  characters.name
+ORDER BY COUNT(*) DESC;"
 end
