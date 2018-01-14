d351f3ec023e868e2ddbcb0144e7ecaaa9a989d2
Blakeley Bond <burrick2003@yahoo.com>
2018-01-14 20:22:53 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES ();
+
+INSERT INTO subgenres (id, name) VALUES ();
+
+INSERT INTO authors (id, name) VALUES ();
+
+INSERT INTO books (id, title, year, series_id) VALUES ();
+
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES ();
+
+INSERT INTO character_books (id, character_id, book_id) VALUES ();
