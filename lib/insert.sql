01e6176bb179e711ccaa4c04af2a902ecec0cf9a
David Towster <djt4xe@virginia.edu>
2018-01-25 09:52:57 -0800

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,47 @@
+INSERT INTO series (title) VALUES
+("title"),
+("title");
+
+INSERT INTO books (title) VALUES
+("title"),
+("title"),
+("title"),
+("title"),
+("title"),
+("title");
+
+INSERT INTO characters (name) VALUES
+("title"),
+("title"),
+("title"),
+("title"),
+("title"),
+("title"),
+("title"),
+("title");
+
+INSERT INTO subgenres (name) VALUES
+("title"),
+("title");
+
+INSERT INTO authors (name) VALUES
+("title"),
+("title");
+
+INSERT INTO character_books (character_id, book_id) VALUES
+(2,2),
+(2,2),
+(2,2),
+(2,2),
+(2,2),
+(2,2),
+(2,2),
+(2,2),
+(2,2),
+(2,2),
+(2,2),
+(2,2),
+(2,2),
+(2,2),
+(2,2),
+(2,2);
