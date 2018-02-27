a5134570223f9ce5a3c280c27fefc55f017c3e86
Chaim Reisman <crreisman@gmail.com>
2018-02-27 02:37:56 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,44 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Funny", 2,7);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Funny", 2,7);
+INSERT INTO subgenres (name) VALUES ('Really!?!?!');
+INSERT INTO subgenres (name) VALUES ('Really!?!?!');
+INSERT INTO authors (name) VALUES ('John Doe');
+INSERT INTO authors (name) VALUES ('John Doe');
+INSERT INTO books (title, year, series_id) VALUES ('Faces', 1625, 1);
+INSERT INTO books (title, year, series_id) VALUES ('Faces', 1625, 1);
+INSERT INTO books (title, year, series_id) VALUES ('Faces', 1625, 1);
+INSERT INTO books (title, year, series_id) VALUES ('Faces', 1625, 2);
+INSERT INTO books (title, year, series_id) VALUES ('Faces', 1625, 2);
+INSERT INTO books (title, year, series_id) VALUES ('Faces', 1625, 2);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES
+('Steve', 'YOLO', 'Rapper', 2, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES
+('Steve', 'YOLO', 'Rapper', 2, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES
+('Steve', 'YOLO', 'Rapper', 2, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES
+('Steve', 'YOLO', 'Rapper', 2, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES
+('Steve', 'YOLO', 'Rapper', 2, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES
+('Steve', 'YOLO', 'Rapper', 2, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES
+('Steve', 'YOLO', 'Rapper', 2, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES
+('Steve', 'YOLO', 'Rapper', 2, 1);
+INSERT INTO character_books (book_id, character_id) VALUES (5,2);
+INSERT INTO character_books (book_id, character_id) VALUES (5,2);
+INSERT INTO character_books (book_id, character_id) VALUES (5,2);
+INSERT INTO character_books (book_id, character_id) VALUES (5,2);
+INSERT INTO character_books (book_id, character_id) VALUES (5,2);
+INSERT INTO character_books (book_id, character_id) VALUES (5,2);
+INSERT INTO character_books (book_id, character_id) VALUES (5,2);
+INSERT INTO character_books (book_id, character_id) VALUES (5,2);
+INSERT INTO character_books (book_id, character_id) VALUES (5,2);
+INSERT INTO character_books (book_id, character_id) VALUES (5,2);
+INSERT INTO character_books (book_id, character_id) VALUES (5,2);
+INSERT INTO character_books (book_id, character_id) VALUES (5,2);
+INSERT INTO character_books (book_id, character_id) VALUES (5,2);
+INSERT INTO character_books (book_id, character_id) VALUES (5,2);
+INSERT INTO character_books (book_id, character_id) VALUES (5,2);
+INSERT INTO character_books (book_id, character_id) VALUES (5,2);
