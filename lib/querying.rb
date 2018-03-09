74f422c68110d6d5f88f13a7615dc2e6a8378137
Carol Greene <carolgreene3@gmail.com>
2018-03-09 23:38:31 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,13 +25,13 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title FROM (
-   SELECT series.title, COUNT(*) FROM series
+
+  "SELECT title FROM series
    JOIN characters
    ON characters.series_id = series.id
    WHERE characters.species = 'human'
-   GROUP BY characters.series_id
-   ORDER BY COUNT(*) DESC limit 1);"
+   GROUP BY series.title
+   ORDER BY COUNT(*) DESC limit 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
