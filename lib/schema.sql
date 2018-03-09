32e561bedf0aef91a93b6772b2148c490a793ea2
Emily Zhao <tong.zhao.92@gmail.com>
2018-03-09 21:21:13 +0000

    Done.

---
+++ lib/schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER
+);
+
+CREATE TABLE subgenres (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year TEXT,
+  series_id INTEGER
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  species TEXT,
+  motto TEXT,
+  series_id INTEGER,
+  author_id INTEGER
+);
+
+CREATE TABLE character_books (
+  book_id INTEGER,
+  character_id INTEGER
+);
