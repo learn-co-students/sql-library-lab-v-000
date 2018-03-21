7b3c7018ec2758e12219a692e0beb8c5e9e5fe00
Andrew Hong <hongandrew16@gmail.com>
2018-03-21 00:05:55 +0000

    #select_name_and_series_subgenres_of_authors can CHAIN INNER JOINS

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,18 +1,29 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year
+  FROM books INNER JOIN series
+  ON books.series_id = series.id
+  WHERE series.id = 1;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto
+  FROM characters
+  ORDER BY length(motto) DESC LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(*)
+  FROM characters
+  GROUP BY characters.species
+  ORDER BY COUNT(species) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+  FROM series
+  INNER JOIN authors ON series.author_id = authors.id
+  INNER JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
