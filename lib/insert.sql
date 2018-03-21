df22daaf67104c5550966520b0c263c35cc7cd15
Jennifer Stoos <jstoos@bitlaw.com>
2018-03-21 01:18:06 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -24,6 +24,13 @@ INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (7, "Character Three", "motto three", "cylon", 2, 2);
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (8, "Character Four", "motto four", "cylon", 2, 2);
 
-INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
+INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1);
+INSERT INTO character_books (id, book_id, character_id) VALUES (2, 1, 2);
+INSERT INTO character_books (id, book_id, character_id) VALUES (3, 2, 2);
+INSERT INTO character_books (id, book_id, character_id) VALUES (4, 3, 2);
+INSERT INTO character_books (id, book_id, character_id) VALUES (5, 1, 3);
+INSERT INTO character_books (id, book_id, character_id) VALUES (6, 2, 3);
+INSERT INTO character_books (id, book_id, character_id) VALUES (7, 3, 3);
+INSERT INTO character_books (id, book_id, character_id) VALUES (8, 1, 4);
 
 INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
