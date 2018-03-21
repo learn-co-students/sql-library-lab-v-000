9984852c2c44bd8e6d11f0e5d32f8a1757afa59c
Andrew Hong <hongandrew16@gmail.com>
2018-03-21 00:49:09 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,9 +27,18 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM series INNER JOIN characters
+  ON series.author_id = characters.author_id
+  ORDER BY characters.species DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(characters.id)
+  FROM character_books
+  INNER JOIN characters ON character_books.character_id = characters.id
+  INNER JOIN books ON character_books.book_id = books.id
+  GROUP BY characters.name
+  ORDER BY COUNT(character_id) DESC
+  ;"
 end
