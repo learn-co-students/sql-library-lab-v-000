ad7c79d9d84d69e5be099bf9d44c54e3e82705fc
Frank McCallion <fkmccallion@gmail.com>
2018-03-22 23:03:29 +0000

    add all schema and start insert statements

---
+++ lib/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO series (title, author_ID, subgenre_ID) VALUES (title1, 1, 1)
+INSERT INTO series (title, author_ID, subgenre_ID) VALUES (title2, 2, 2)
---
+++ lib/schema.sql
@@ -0,0 +1,32 @@
+CREATE TABLE series
+  (id INTEGER PRIMARY KEY,
+    title TEXT,
+    author_ID INTEGER,
+    subgenre_ID INTEGER);
+
+CREATE TABLE subgenres
+  (id INTEGER PRIMARY KEY,
+    name TEXT);
+
+CREATE TABLE authors
+  (id INTEGER PRIMARY KEY,
+    name TEXT);
+
+CREATE TABLE books
+  (id INTEGER PRIMARY KEY,
+    title TEXT,
+    year INTEGER,
+    series_ID INTEGER);
+
+CREATE TABLE character_books
+  (id INTEGER PRIMARY KEY,
+    character_ID INTEGER,
+    book_ID INTEGER);
+
+CREATE TABLE characters
+  (id INTEGER PRIMARY KEY,
+    name TEXT,
+    motto TEXT,
+    species TEXT,
+    author_ID INTEGER,
+    series_ID INTEGER);
