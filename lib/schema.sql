c2cc1fec57edccfa4b5dedbc093bb4900216e138
Joseph Parker <jparker010190@gmail.com>
2018-04-05 01:05:47 +0000

    Done.

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,4 +1,38 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title text,
+  author_id integer,
+  subgenre_id integer
+);
+
+CREATE TABLE subgenres(
+name text,
+id INTEGER primary key
+);
+
+CREATE TABLE authors(
+name text,
+id INTEGER primary key
+);
+
+CREATE TABLE books(
+title text,
+year integer,
+series_id integer,
+id INTEGER primary key
+);
+
 CREATE TABLE characters (
-  id INTERGER PRIMARY KEY, 
-  series 
+name text,
+species text,
+motto text,
+series_id integer,
+author_id integer,
+id INTEGER primary key
+);
+
+CREATE TABLE character_books(
+id INTEGER primary key,
+book_id integer,
+character_id integer
 );
