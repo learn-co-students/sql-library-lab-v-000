6d331e2e42c4229f5e007fddf4023f9359cfe856
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-05-02 16:17:37 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,18 +1,28 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title, year 
+  FROM books 
+  WHERE books.series_id = 1 
+  ORDER BY year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto 
+  FROM characters
+  ORDER BY motto LIMIT 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT DISTINCT species, COUNT(species)
+  FROM characters
+  GROUP BY species
+  ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, series.subgenres
+  FROM authors LEFT OUTER JOIN series
+  ON authors.id = series.author_id"
 end
 
 def select_series_title_with_most_human_characters
