777933705537b089e83267302338d406a08fbef2
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-10 14:18:53 +0000

    Done.

--- lib/update.sql
+++ lib/update.sql
@@ -1,10 +1,2 @@
--- WITH c AS(
--- SELECT species
--- FROM characters
--- ORDER BY id DESC
--- LIMIT 1)
--- UPDATE c SET 'Martin'  ;
 
--- with c AS(
--- SELECT species FROM characters ORDER BY ID DESC LIMIT 1)
 UPDATE characters SET species = 'Martian' WHERE species = 'cylon';
