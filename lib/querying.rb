4e692124ba365cde45b83b11827630e386e74bf1
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-13 21:37:15 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,10 +12,11 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT subgenres.name, authors.name
+  "SELECT  authors.name, subgenres.name
   FROM subgenres, authors
   INNER JOIN series
   ON subgenres.id = authors.id
+  GROUP BY subgenres.name
   "
 end
 
