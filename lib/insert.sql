94a58ac72b2fc88dd71c2dcbdfe9e05fcad61daa
Ian Vlahovic <banddorkfht7@gmail.com>
2018-05-26 03:37:44 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Lord of the Rings", 2, 2);
+INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "fantasy");
+INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "J.R.R. Tolkien");
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "The Fellowship of the Ring", 1954, 2), (5, "The Two Towers", 1954, 2), (6, "The Return of the King", 1955, 2);
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Nymeria", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lannister", "That's what I do: I drink, and I know things", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Frodo Baggins", "It is useless to meet revenge with revenge: it will heal nothing", "hobbit", 2, 2), (6, "Gandalf the Grey", "A wizard is never late, nor is he early...he arrives precisely when he means to", "wizard", 2, 2), (7, "Aragorn, High King of Gondor and Arnor", "If by my life or death I can protect you, I will", "human", 2, 2), (8, "Legolas", "Oft hope is born when all is forlorn", "elf", 2, 2);
+INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
+INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
