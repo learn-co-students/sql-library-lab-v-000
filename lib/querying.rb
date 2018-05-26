826a7865fa4a8fa7c50ea256e114a41d030ecf8b
Christopher P. Dundon <chris.dundon@gmail.com>
2018-05-25 22:27:10 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,13 +12,13 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT DISTINCT authors.name AS author, subgenres.name AS subgenre FROM authors JOIN series ON authors.id = series.author_id JOIN subgenres ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series JOIN authors ON series.author_id = authors.id JOIN characters ON authors.id = characters.author_id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(characters.species) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name AS name, COUNT(*) FROM characters JOIN character_books ON characters.id = character_books.character_id GROUP BY characters.name ORDER BY COUNT(characters.name) DESC"
 end
