82173b77a914f959c7dc87d48fb5da1f9c509a5e
David McGrann <dpmcgrann@gmail.com>
2018-07-08 18:16:23 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,15 +1,14 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year
-    FROM series
-    INNER JOIN books ON books.series_id = series.id
-    WHERE books.series_id = 1
-    ORDER BY books.year ASC"
+  "SELECT title, year
+    FROM books
+    WHERE series_id = 1
+    ORDER BY year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto
     FROM characters
-    ORDER by characters.motto ASC
+    ORDER by motto ASC
     LIMIT 1"
 end
 
@@ -25,15 +24,15 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
     FROM series
-    INNER JOIN authors ON series.author_id = authors.id
-    INNER JOIN subgenres ON series.subgenre_id = subgenres.id
+    JOIN authors ON series.author_id = authors.id
+    JOIN subgenres ON series.subgenre_id = subgenres.id
     GROUP BY series.id"
 end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title
     FROM characters
-    INNER JOIN series ON characters.series_id = series.id
+    JOIN series ON characters.series_id = series.id
     WHERE characters.species = 'human'
     GROUP BY series.title
     ORDER BY characters.species DESC
@@ -41,9 +40,9 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books.character_id) AS occurences
+  "SELECT characters.name, COUNT(*) AS occurences
     FROM characters
-    INNER JOIN character_books ON character_books.character_id = characters.id
+    JOIN character_books ON character_books.character_id = characters.id
     GROUP BY characters.name
     ORDER BY occurences DESC"
 end
