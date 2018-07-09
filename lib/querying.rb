0e888f84ded16c09423433d8acc0e7415d73b9e4
Hayden Betts <haydenbetts@gmail.com>
2018-07-09 23:08:48 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,14 +1,25 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "select books.title, books.year
+  FROM books
+  INNER JOIN series
+  ON books.series_id = series.id
+  WHERE series.id = 1;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "select characters.name, characters.motto
+  FROM characters
+  ORDER BY length(characters.motto) DESC
+  LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "select characters.species, count(characters.id)
+  FROM characters
+  GROUP BY characters.species
+  ORDER BY count(characters.id) DESC
+  LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
