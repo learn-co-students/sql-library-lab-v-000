b1106501d7ff812934e10732b7c14d5f715e4087
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-07-24 02:33:25 +0000

    Done.

---
+++ lib/schema.sql
@@ -0,0 +1,32 @@
+CREATE TABLE series (
+  id PRIMARY KEY INTEGER,
+  title TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER
+);
+
+CREATE TABLE books (
+  id PRIMARY KEY INTEGER,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE authors (
+  id PRIMARY KEY INTEGER,
+  name TEXT
+);
+
+CREATE TABLE characters (
+  id PRIMARY KEY INTEGER,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE subgenres (
+  id PRIMARY KEY INTEGER,
+  name TEXT
+);
