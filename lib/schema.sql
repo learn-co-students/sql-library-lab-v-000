b6378f63c25fae5fbfa22b6f2c34ba585d92a158
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-07-26 18:29:43 +0000

    Done.

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,6 +1,6 @@
-CREATE TABLE series (id INTEGER PRIMARY KEY, title, author_id, subgenre_id);
-CREATE TABLE subgenres (id INTEGER PRIMARY KEY, name);
-CREATE TABLE authors (id INTEGER PRIMARY KEY, name);
-CREATE TABLE books (id INTEGER PRIMARY KEY, title, year, series_id);
-CREATE TABLE characters (id INTEGER PRIMARY KEY, name, motto, species, author_id, series_id);
-CREATE TABLE character_books (id INTEGER PRIMARY KEY, book_id, character_id);
+CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER);
+CREATE TABLE subgenres (id INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
+CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER, series_id INTEGER);
+CREATE TABLE character_books (id INTEGER PRIMARY KEY, book_id INTEGER, character_id INTEGER);
