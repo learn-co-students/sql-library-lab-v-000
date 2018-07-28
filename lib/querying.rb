c81afdd188f46f31fd547f8742f75b143f4c90f3
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-07-28 20:39:43 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,9 +15,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series INNER JOIN characters ON series.id = characters.series_id WHERE characters.species = \"human\" ORDER BY characters.species DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(character_books.book_id) FROM characters LEFT OUTER JOIN character_books ON characters.id = character_books.character_id GROUP BY characters.name ORDER BY COUNT(character_books.book_id) DESC;"
 end
