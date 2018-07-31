c2a999675f4c2d99e0df89cb444cd7c38cdcd98f
Dani Schuhman <dani8439@aol.com>
2018-07-31 18:34:03 +0000

    second pass

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,6 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books WHERE books.series_id = 1 ORDER BY books.year ASC;"
+  "SELECT books.title, books.year FROM books WHERE series_id = 1 ORDER BY books.year ASC;"
+  # SELECT books.title, books.year FROM books WHERE series_id = 1 also worked
 end
 
 def select_name_and_motto_of_char_with_longest_motto
@@ -13,10 +14,11 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors JOIN subgenres ON authors.id = subgenres.id;"
+
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN characters ON characters.series_id = series.id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(characters.species) DESC LIMIT 1;"
+  "SELECT series.title FROM series JOIN characters ON series.id = characters.series_id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT (characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
