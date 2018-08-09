94d34632238ea7edaddd1f4bcb9f9fb339dc6015
Will Schultz <will.j.schultz@gmail.com>
2018-08-09 19:47:50 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,6 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   <<-SQL
-  SELECT title, year
+    SELECT title, year
     FROM books
     WHERE series_id=1
     ORDER BY year
@@ -8,12 +8,13 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto
+  <<-SQL
+    SELECT name, motto
     FROM characters
     ORDER BY LENGTH(motto) DESC
-    LIMIT 1"
+    LIMIT 1
+  SQL
 end
-
 
 def select_value_and_count_of_most_prolific_species
   <<-SQL
@@ -26,16 +27,19 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name
+  <<-SQL
+    SELECT authors.name, subgenres.name
     FROM authors
     JOIN series
     ON series.author_id = authors.id
     JOIN subgenres
-    ON series.subgenre_id = subgenres.id"
+    ON series.subgenre_id = subgenres.id
+  SQL
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title
+  <<-SQL
+    SELECT series.title
     FROM series
     JOIN books
     ON books.series_id = series.id
@@ -46,14 +50,17 @@ def select_series_title_with_most_human_characters
     WHERE characters.species = 'human'
     GROUP BY series.title
     ORDER BY COUNT(*) DESC
-    LIMIT 1"
+    LIMIT 1
+  SQL
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(*) as book_count
+  <<-SQL
+    SELECT characters.name, COUNT(*) as book_count
     FROM character_books
     JOIN characters
     ON character_books.character_id = characters.id
     GROUP BY characters.name
-    ORDER BY book_count DESC"
+    ORDER BY book_count DESC
+  SQL
 end
