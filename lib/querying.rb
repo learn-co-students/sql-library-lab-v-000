6eb705196038d5aa7776966c37f4aadf559eafc1
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-14 19:00:28 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,8 +20,9 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
-  INNER JOIN subgenres
-  ON authors.id = series.author_id AND subgenres.id = series.subgenre_id;"
+  INNER JOIN series ON authors.id = series.author_id
+  INNER JOIN subgenres ON subgenres.id = series.subgenre_id;"
+
 end
 
 def select_series_title_with_most_human_characters
