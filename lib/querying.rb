5d87982f58d3a1fd516b47825a7cee60dd5d9cbe
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-14 23:22:45 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,9 +28,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, characters.species
+  "SELECT series.title
   FROM series
-  INNER JOIN characters ON characters.species = "human"
+  INNER JOIN characters ON characters.species = 'human'
   GROUP BY series.title
   ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
