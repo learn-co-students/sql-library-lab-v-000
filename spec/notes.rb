efdef995109673efb8c4fcff9f86e085a0d696e5
Marcus  = Soulo <souloinc87@gmail.com>
2018-08-23 12:10:43 +0000

    Done.

---
+++ spec/notes.rb
@@ -0,0 +1,6 @@
+#has 2 series (FAILED - 18)
+  #has 6 books (FAILED - 19)
+  #has 8 characters (FAILED - 20)
+  #has 2 subgenres (FAILED - 21)
+  #has 2 authors (FAILED - 22)
+  has 16 joins in character_books (FAILED - 23)
