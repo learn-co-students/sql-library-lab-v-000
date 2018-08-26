10f72e4153bc5ba0a446a391da7854fd3e75f222
AT Williams <aderiannaw@yahoo.com>
2018-08-26 22:27:28 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -40,10 +40,10 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(*) AS book_count
+  "SELECT characters.name, COUNT(*) AS book_num
    FROM characters
    JOIN character_books 
    ON characters.id = character_books.character_id
    GROUP BY characters.name 
-   Order BY book_count DESC;"
+   Order BY book_num DESC;"
 end
