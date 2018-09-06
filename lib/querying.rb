9c7108e96747d87af97e884f105f74f19c27e0bf
Alex Roman <elpitbull6989@gmail.com>
2018-09-06 07:34:35 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,33 +1,30 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year
-  FROM books
-  WHERE series_id = 1"
+  "SELECT title, year FROM books WHERE series_id = 1"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto
-  FROM characters ORDER BY motto Limit 1"
+  "SELECT name, motto FROM characters ORDER BY motto limit 1"
 end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species)
-  FROM characters GROUP BY species
-  ORDER BY COUNT(species) DESC Limit 1"
+   FROM characters GROUP BY species
+   ORDER BY COUNT(species) DESC limit 1"
+
 end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
-  FROM authors INNER JOIN series ON authors.id = series.author_id
-  INNER JOIN subgenres ON subgenres.id = series.subgenre_id "
+   FROM authors JOIN series ON authors.id = series.author_id
+   JOIN subgenres ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title
-  FROM series
-  INNER JOIN characters ON characters.series_id = series.id
-  WHERE characters.species = 'human'
-  GROUP by series.title
-  ORDER BY COUNT(species) DESC Limit 1"
+  "SELECT series.title FROM series
+   JOIN characters ON series.id = characters.series_id
+   WHERE characters.species = 'human'
+   GROUP BY series.title
+   ORDER BY COUNT(species) DESC limit 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
@@ -35,6 +32,6 @@ def select_character_names_and_number_of_books_they_are_in
   FROM characters
   INNER JOIN character_books ON characters.id = character_books.character_id
   GROUP by characters.name
-  ORDER by COUNT(character_books.book_id) DESC
-  "
+  ORDER by COUNT(character_books.book_id) DESC"
+
 end
