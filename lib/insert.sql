3a9d31bd1decd4809ad03ade505b02be8e106b17
github username <github email address>
2018-09-24 21:29:01 -0600

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -20,27 +20,18 @@ INSERT INTO characters (name, motto, species, author_id, series_id)
 INSERT INTO characters (name, motto, species, author_id, series_id)
   VALUES ("Samwise Gamgee", "Right, and I'm coming with you!", "Hobbit", 1, 1);
 INSERT INTO characters (name, motto, species, author_id, series_id)
-  VALUES ("Gandalf", "You shall not pass!", "Wizard", "Human", 1, 1);
+  VALUES ("Gandalf", "You shall not pass!", "Human", 1, 1);
 INSERT INTO characters (name, motto, species, author_id, series_id)
   VALUES ("Gimli", "Never toss a dwarf!", "Dwarf", 1, 1);
 
 INSERT INTO characters (name, motto, species, author_id, series_id)
   VALUES ("Paul Atreides", "We pay for the violence of our ancestors", "Human", 2, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id)
-  VALUES ("Lady Jessica", "Beginnings are times of such peril.", 2, 2);
+  VALUES ("Lady Jessica", "Beginnings are times of such peril.", "Human", 2, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id)
-  VALUES ("Gurney Halleck", "The slow blade penetrates the shield.", 2, 2);
+  VALUES ("Gurney Halleck", "The slow blade penetrates the shield.", "Human", 2, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id)
-    VALUES ("Baron Harkonnen", "The spice must flow!", 2, 2);
-
-INSERT INTO character_books (character_id, book_id) VALUES (1, 1); --Frodo in all three
-INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
-INSERT INTO character_books (character_id, book_id) VALUES (1, 3);
-INSERT INTO character_books (character_id, book_id) VALUES (2, 1); --Sam in all three
-INSERT INTO character_books (character_id, book_id) VALUES (2, 2);
-INSERT INTO character_books (character_id, book_id) VALUES (2, 3);
-INSERT INTO character_books (character_id, book_id) VALUES (3, 1); --Gandalf in first
-INSERT INTO character_books (character_id, book_id) VALUES (4, 1); --Gimli in first
+    VALUES ("Baron Harkonnen", "The spice must flow!", "Human", 2, 2);
 
 INSERT INTO character_books (character_id, book_id) VALUES (1, 1); --Frodo in all three
 INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
@@ -57,6 +48,5 @@ INSERT INTO character_books (character_id, book_id) VALUES (5, 6);
 INSERT INTO character_books (character_id, book_id) VALUES (6, 4); --Jessica in all three
 INSERT INTO character_books (character_id, book_id) VALUES (6, 5);
 INSERT INTO character_books (character_id, book_id) VALUES (6, 6);
-
 INSERT INTO character_books (character_id, book_id) VALUES (7, 4); --Gurney in first
 INSERT INTO character_books (character_id, book_id) VALUES (8, 4); --Barron in first
