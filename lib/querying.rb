ea1b1ed1074b4316fd68a3f656c1545bfbb2b9c1
github username <github email address>
2018-09-25 09:01:50 -0600

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -7,20 +7,39 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto
+  FROM characters
+  ORDER BY length(motto) DESC
+  LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(*)
+  FROM characters
+  GROUP BY species
+  ORDER BY COUNT(*) DESC
+  LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+  FROM authors
+  JOIN series ON authors.id = series.author_id
+  JOIN subgenres ON subgenres.id = series.subgenre_id"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title --, books.title, character_books.character_id, characters.name, characters.species
+  FROM series
+  JOIN books ON series.id = books.series_id
+  JOIN character_books ON books.id = character_books.book_id
+  JOIN characters ON characters.id = character_books.character_id
+  WHERE characters.species = 'human'
+  GROUP BY series.id
+  ORDER BY count(*)DESC
+  LIMIT 1
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
