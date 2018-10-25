c59c97fefce26fdfc2cc52da0c4d4fd1260674c5
Harley Rayner <harleymteden@gmail.com>
2018-10-25 06:05:10 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,9 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT Series.title, Books.year
+   FROM series
+   LEFT JOIN books
+   ON Series.id = Book.series_id
+   Limit 1;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
