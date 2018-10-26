c337c6deaf0ad886d2bdfa70687648588d3a5d5c
Harley Rayner <harleymteden@gmail.com>
2018-10-26 18:11:54 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,35 +3,64 @@ def select_books_titles_and_years_in_first_series_order_by_year
    FROM series
    INNER JOIN books
    ON Series.id = Books.series_id
-   ORDER BY Books.year
-   LIMIT 3;"
+   WHERE Series.id = 1
+   ORDER BY Books.year"
 end
-
-#above method is sketchy because 3 books is hard-coded
-
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT Characters.name, Characters.motto
    FROM Characters
-   ORDER BY Characters.motto
-   LIMIT 1;"
+   ORDER BY LENGTH(Characters.motto)
+   DESC LIMIT 1;"
 end
 
-#another sketchy hard-coded method above...
-
-
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT Characters.species, COUNT(Characters.species)
+   FROM Characters
+   GROUP BY species
+   ORDER BY COUNT(species)
+   DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT Authors.name, Subgenres.name
+   FROM Authors
+   LEFT JOIN Series
+   ON Series.author_id = Authors.id
+   JOIN Subgenres
+   WHERE Subgenres.id = Series.subgenre_id
+   GROUP BY Authors.name"
 end
 
+# def select_series_title_with_most_human_characters
+#   "SELECT Series.title
+#    FROM Series
+#    JOIN Characters
+#    ON Series.id = Characters.series_id
+#    WHERE Characters.species = 'human'
+#    GROUP BY species
+#    ORDER BY COUNT(species)
+#    "
+# end
+
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT Series.title
+   FROM Series
+   JOIN Characters
+   ON Series.id = Characters.series_id
+   WHERE Characters.species = 'human'
+   GROUP BY Series.title
+   ORDER BY COUNT(species)
+   DESC LIMIT 1;
+   "
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT Characters.name, COUNT(Character_books.book_id)
+   FROM Characters
+   JOIN Character_books
+   ON Characters.id = Character_books.character_id
+   GROUP BY Characters.name
+   ORDER BY COUNT(Character_books.book_id) DESC
+   "
 end
