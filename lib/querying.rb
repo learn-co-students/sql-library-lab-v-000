177f9d9c0efd8b6a972db85a38c577f1d9752373
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-28 20:12:29 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,12 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT "
+  "SELECT authors.name, subgenres.name
+  FROM series 
+  JOIN series 
+  ON series.subgenres_id = subgenres.id
+  JOIN series
+  ON series.author_id = author.id;"
 end
 
 def select_series_title_with_most_human_characters
