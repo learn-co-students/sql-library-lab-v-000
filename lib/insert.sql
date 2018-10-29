8167aba2fe30e369c57f2d5ae8e177d4ca759492
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-29 20:54:38 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,3 +7,8 @@ INSERT INTO characters (id, name, species, motto, series_id, author_id) VALUES (
  (6, 'hello', 'hello', 'hello', 1, 2), (7, 'hello', 'hello', 'hello', 1, 2), (8, 'hello', 'hello', 'hello', 1, 2);
 
 INSERT INTO subgenres (id, name) VALUES (1, 'hello'), (2, 'hello');
+
+INSERT INTO authors (id, name) VALUES (1, 'hello'), (2, 'hello');
+
+INSERT INTO character_books (id, book_id, character_id) VALUES (1, 2, 3), (2, 2, 3), (3, 2, 3), (4, 2, 3), (5, 2, 3), (6, 2, 3), (7, 2, 3), (8, 2, 3), (9, 2, 3), (10, 2, 3), (11, 2, 3),
+(12, 2, 3), (13, 2, 3), (14, 2, 3), (15, 2, 3), (16, 2, 3);
