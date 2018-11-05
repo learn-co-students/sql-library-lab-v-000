7817e1b37e5b5f1fd6ba1aeab2706c66ea145203
meg-gutshall <meghan.gutshall@gmail.com>
2018-11-05 16:23:25 -0500

    Reformat SQL statement strings

--- lib/querying.rb
+++ lib/querying.rb
@@ -7,17 +7,46 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) AS x FROM characters GROUP BY species ORDER BY x DESC LIMIT 1;"
+  <<-SQL
+    SELECT species, COUNT(species) 
+    FROM characters 
+    GROUP BY species 
+    ORDER BY COUNT(species) DESC 
+    LIMIT 1;
+  SQL
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM series JOIN authors ON series.author_id = authors.id JOIN subgenres ON series.subgenre_id = subgenres.id;"
+  <<-SQL
+    SELECT authors.name, subgenres.name 
+    FROM series 
+    JOIN authors 
+    ON series.author_id = authors.id 
+    JOIN subgenres 
+    ON series.subgenre_id = subgenres.id;
+  SQL
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN characters ON series.id = characters.series_id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(characters.id) DESC LIMIT 1;"
+  <<-SQL
+    SELECT series.title 
+    FROM series 
+    JOIN characters 
+    ON series.id = characters.series_id 
+    WHERE characters.species = 'human' 
+    GROUP BY series.title 
+    ORDER BY COUNT(characters.id) DESC 
+    LIMIT 1;
+  SQL
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books.book_id) FROM characters JOIN character_books ON characters.id = character_books.character_id GROUP BY characters.name ORDER BY COUNT(character_books.book_id) DESC;"
+  <<-SQL
+    SELECT characters.name, COUNT(character_books.book_id) 
+    FROM characters 
+    JOIN character_books 
+    ON characters.id = character_books.character_id 
+    GROUP BY characters.name 
+    ORDER BY COUNT(character_books.book_id) DESC;
+  SQL
 end
