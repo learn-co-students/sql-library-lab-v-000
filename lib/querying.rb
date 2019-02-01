d7bc22d929c85b7a27fdda738eddc324e6085d57
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-02-01 00:49:29 +0000

    wip

--- lib/querying.rb
+++ lib/querying.rb
@@ -7,12 +7,17 @@ ORDER BY books.year ASC;
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto FROM characters
+  ORDER BY LENGTH(characters.motto) DESC LIMIT 1;
+  "
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(*) FROM characters
+  GROUP BY characters.species
+  ORDER BY characters.species DESC LIMIT 1;
+"
 end
 
 def select_name_and_series_subgenres_of_authors
