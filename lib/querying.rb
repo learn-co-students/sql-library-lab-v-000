f249dc8f0d3e0afba06f9a7ccbfb080ae7cc7047
Chao Hsuan Chi <allen0413@gmail.com>
2019-02-17 03:58:03 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,12 +3,12 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT characters.name, characters.motto FROM characters WHERE characters.id = 2;"
+  "SELECT characters.name, characters.motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species) FROM characters ORDER BY COUNT(characters.species) DESC LIMIT 1;"
+  "SELECT characters.species, COUNT(characters.species) FROM characters GROUP BY species ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
