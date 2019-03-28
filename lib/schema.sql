981e4ba795f69b930b53a633bc51d224dafd26b5
Karly Mareka <karly.mareka@gmail.com>
2019-03-28 02:12:11 +0000

    Done.

--- lib/schema.sql
+++ lib/schema.sql
@@ -18,5 +18,15 @@ name TEXT
 CREATE TABLE books(
 id INTEGER PRIMARY KEY,
 title TEXT,
-year INTEGER
+year INTEGER,
+series_id INTEGER
+);
+
+CREATE TABLE characters(
+id INTEGER PRIMARY KEY,
+name TEXT,
+species TEXT,
+motto TEXT,
+series_id INTEGER,
+author_id INTEGER
 );
