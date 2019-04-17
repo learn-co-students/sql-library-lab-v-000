7e7d52bc9a49015db83b9ee4c3e65b3a7778a9ed
Justin Hall <hall.justin.d@gmail.com>
2019-04-17 02:40:50 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,71 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES (null, null, null);
+
+INSERT INTO series (title, author_id, subgenre_id) VALUES (null, null, null);
+
+INSERT INTO books (title, year, series_id) VALUES (null, null, null);
+
+INSERT INTO books (title, year, series_id) VALUES (null, null, null);
+
+INSERT INTO books (title, year, series_id) VALUES (null, null, null);
+
+INSERT INTO books (title, year, series_id) VALUES (null, null, null);
+
+INSERT INTO books (title, year, series_id) VALUES (null, null, null);
+
+INSERT INTO books (title, year, series_id) VALUES (null, null, null);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES (null, null, null, null, null);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES (null, null, null, null, null);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES (null, null, null, null, null);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES (null, null, null, null, null);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES (null, null, null, null, null);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES (null, null, null, null, null);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES (null, null, null, null, null);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES (null, null, null, null, null);
+
+INSERT INTO subgenres (name) VALUES (null);
+
+INSERT INTO subgenres (name) VALUES (null);
+
+INSERT INTO authors (name) VALUES (null);
+
+INSERT INTO authors (name) VALUES (null);
+
+INSERT INTO character_books (character_id, book_id) VALUES (null, null);
+
+INSERT INTO character_books (character_id, book_id) VALUES (null, null);
+
+INSERT INTO character_books (character_id, book_id) VALUES (null, null);
+
+INSERT INTO character_books (character_id, book_id) VALUES (null, null);
+
+INSERT INTO character_books (character_id, book_id) VALUES (null, null);
+
+INSERT INTO character_books (character_id, book_id) VALUES (null, null);
+
+INSERT INTO character_books (character_id, book_id) VALUES (null, null);
+
+INSERT INTO character_books (character_id, book_id) VALUES (null, null);
+
+INSERT INTO character_books (character_id, book_id) VALUES (null, null);
+
+INSERT INTO character_books (character_id, book_id) VALUES (null, null);
+
+INSERT INTO character_books (character_id, book_id) VALUES (null, null);
+
+INSERT INTO character_books (character_id, book_id) VALUES (null, null);
+
+INSERT INTO character_books (character_id, book_id) VALUES (null, null);
+
+INSERT INTO character_books (character_id, book_id) VALUES (null, null);
+INSERT INTO character_books (character_id, book_id) VALUES (null, null);
+
+INSERT INTO character_books (character_id, book_id) VALUES (null, null);
+
